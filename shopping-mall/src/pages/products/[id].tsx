1c66a2e99b86e0188ceecbd6117da0de8129ca99
ParkJeongHo <pjh31208@naver.com>
2022-12-20 20:37:28 +0900

    feat : Data fetching with react query and graphQL

--- shopping-mall/src/pages/products/[id].tsx
+++ shopping-mall/src/pages/products/[id].tsx
@@ -1,5 +1,18 @@
-const ProductDetail = () => {
-  return <div>상세페이지</div>;
+import { useQuery } from "react-query";
+import { graphqlFetcher, QueryKeys } from "../../queryClient";
+import { useParams } from "react-router-dom";
+import ProductDetail from "../../components/product/detail";
+import { GET_PRODUCT, Product } from "../../graphql/products";
+
+const ProductDetailPage = () => {
+  const { id } = useParams();
+  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
+    graphqlFetcher(GET_PRODUCT, { id })
+  );
+
+  if (!data) return null;
+
+  return <ProductDetail item={data} />;
 };
 
-export default ProductDetail;
+export default ProductDetailPage;
