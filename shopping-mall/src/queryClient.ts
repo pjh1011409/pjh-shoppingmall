54b7ff90a757d5fce58f4d53afe621c5c30236e5
ParkJeongHo <pjh31208@naver.com>
2022-12-20 20:20:36 +0900

    feat : Create react query related function
    QueryClient, Fetcher,  QueryKeys 생성

---
+++ shopping-mall/src/queryClient.ts
@@ -0,0 +1,70 @@
+import { QueryClient } from "react-query";
+import { request, RequestDocument } from "graphql-request";
+
+// Create a client
+export const getClient = (() => {
+  let client: QueryClient | null = null;
+  return () => {
+    if (!client)
+      client = new QueryClient({
+        defaultOptions: {
+          queries: {
+            cacheTime: 1000 * 60 * 60 * 24,
+            staleTime: 1000 * 60,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      });
+    return client;
+  };
+})();
+
+const BASE_URL = "https://fakestoreapi.com";
+
+export const fetcher = async ({
+  method,
+  path,
+  body,
+  params,
+}: {
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  path: string;
+  body?: any;
+  params?: any;
+}) => {
+  try {
+    let url = `${BASE_URL}${path}`;
+    const fetchOptions: RequestInit = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": BASE_URL,
+      },
+    };
+
+    if (params) {
+      const searchParams = new URLSearchParams(params);
+      url += "?" + searchParams.toString();
+    }
+    if (body) fetchOptions.body = JSON.stringify(body);
+
+    const res = await fetch(url, fetchOptions);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
+  request(`${BASE_URL}/graphql`, query, variables, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": BASE_URL,
+  });
+
+export const QueryKeys = {
+  PRODUCTS: "PRODUCTS",
+  CART: "CART",
+};
