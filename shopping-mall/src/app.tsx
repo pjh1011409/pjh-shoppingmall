0c4b318e3d20347af9130a5f2728777581b1ff67
ParkJeongHo <pjh31208@naver.com>
2022-12-20 20:28:38 +0900

    feat : Apply global Providers and Components

--- shopping-mall/src/app.tsx
+++ shopping-mall/src/app.tsx
@@ -1,10 +1,20 @@
 import { useRoutes } from "react-router-dom";
 import { routes } from "./routes";
+import { getClient } from "./queryClient";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import NavBar from "./components/common/navBar";
 
 const App = () => {
   const elem = useRoutes(routes);
-
-  return <div>{elem}</div>;
+  const queryClient = getClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavBar />
+      {elem}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
