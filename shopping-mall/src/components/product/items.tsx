f378a789b983695cdf8863cd42a97fc40e621f6d
ParkJeongHo <pjh31208@naver.com>
2022-12-22 17:10:44 +0900

    feat : Add product to cart
    useMutation을 통해 장바구니에 추가된 데이터 업데이트

--- shopping-mall/src/components/product/items.tsx
+++ shopping-mall/src/components/product/items.tsx
@@ -1,7 +1,14 @@
 import { Product } from "../../graphql/products";
 import { Link } from "react-router-dom";
+import { useMutation } from "react-query";
+import { graphqlFetcher } from "../../queryClient";
+import { ADD_CART } from "../../graphql/cart";
 
 const ProductItem = ({ id, imageUrl, price, title }: Product) => {
+  const { mutate: addCart } = useMutation((id: string) =>
+    graphqlFetcher(ADD_CART, { id })
+  );
+
   return (
     <>
       <li className="product-item">
@@ -10,6 +17,9 @@ const ProductItem = ({ id, imageUrl, price, title }: Product) => {
           <img src={imageUrl} className="product-item__image" />
           <span>${price}</span>
         </Link>
+        <button className="product-item__add-cart" onClick={() => addCart(id)}>
+          담기
+        </button>
       </li>
     </>
   );
