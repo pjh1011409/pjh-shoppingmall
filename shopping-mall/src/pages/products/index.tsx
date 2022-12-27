80c9e6dbe3ea094f1d84f0bc1e17503c3f8570f7
ParkJeongHo <pjh31208@naver.com>
2022-12-27 18:30:43 +0900

    refector : Component segmentation for product pages

--- shopping-mall/src/pages/products/index.tsx
+++ shopping-mall/src/pages/products/index.tsx
@@ -1,21 +1,20 @@
 import { useQuery } from "react-query";
-import ProductItem from "../../components/product/items";
+import ProductList from "../../components/product/index";
 import GET_PRODUCTS from "../../graphql/products";
 import { graphqlFetcher, QueryKeys } from "../../queryClient";
 import { Products } from "../../graphql/products";
 
-const ProductList = () => {
+const ProductListPage = () => {
   const { data } = useQuery<Products>(QueryKeys.PRODUCTS, () =>
     graphqlFetcher(GET_PRODUCTS)
   );
 
   return (
-    <ul className="products">
-      {data?.products.map((product) => (
-        <ProductItem {...product} key={product.id} />
-      ))}
-    </ul>
+    <div>
+      <h2>상품목록</h2>
+      <ProductList list={data?.products || []} />
+    </div>
   );
 };
 
-export default ProductList;
+export default ProductListPage;
