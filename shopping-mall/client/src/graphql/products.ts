778549a394f73f67716af319a39ca20329b377c6
ParkJeongHo <pjh31208@naver.com>
2022-12-29 17:24:01 +0900

    feat : Create graphql for Add, Update and delete products

--- shopping-mall/client/src/graphql/products.ts
+++ shopping-mall/client/src/graphql/products.ts
@@ -9,13 +9,15 @@ export type Product = {
   createdAt: number;
 };
 
+export type MutableProduct = Omit<Product, "id" | "createdAt">; // 지정한 타입을 뺀 나머지 타입.
+
 export type Products = {
   products: Product[];
 };
 
 const GET_PRODUCTS = gql`
-  query GET_PRODUCTS($cursor: ID) {
-    products(cursor: $cursor) {
+  query GET_PRODUCTS($cursor: ID, $showDeleted: Boolean) {
+    products(cursor: $cursor, showDeleted: $showDeleted) {
       id
       imageUrl
       price
@@ -25,7 +27,6 @@ const GET_PRODUCTS = gql`
     }
   }
 `;
-export default GET_PRODUCTS;
 
 export const GET_PRODUCT = gql`
   query GET_PRODUCT($id: ID!) {
@@ -39,3 +40,59 @@ export const GET_PRODUCT = gql`
     }
   }
 `;
+
+export const ADD_PRODUCT = gql`
+  mutation ADD_PRODUCT(
+    $imageUrl: String!
+    $price: Int!
+    $title: String!
+    $description: String!
+  ) {
+    addProduct(
+      imageUrl: $imageUrl
+      price: $price
+      title: $title
+      description: $description
+    ) {
+      id
+      imageUrl
+      price
+      title
+      description
+      createdAt
+    }
+  }
+`;
+
+export const UPDATE_PRODUCT = gql`
+  mutation UPDATE_PRODUCT(
+    $id: ID!
+    $imageUrl: String
+    $price: Int
+    $title: String
+    $description: String
+  ) {
+    updateProduct(
+      id: $id
+      imageUrl: $imageUrl
+      price: $price
+      title: $title
+      description: $description
+    ) {
+      id
+      imageUrl
+      price
+      title
+      description
+      createdAt
+    }
+  }
+`;
+
+export const DELETE_PRODUCT = gql`
+  mutation DELETE_PRODUCT($id: ID!) {
+    deleteProduct(id: $id)
+  }
+`;
+
+export default GET_PRODUCTS;
