0f5406f4133c8ef7d1f3ec68234495cbfe96cf27
ParkJeongHo <pjh31208@naver.com>
2022-12-29 10:25:45 +0900

    feat : infinite scroll implementation

--- shopping-mall/client/src/pages/products/index.tsx
+++ shopping-mall/client/src/pages/products/index.tsx
@@ -1,18 +1,38 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import ProductList from "../../components/product/index";
 import GET_PRODUCTS from "../../graphql/products";
 import { graphqlFetcher, QueryKeys } from "../../queryClient";
 import { Products } from "../../graphql/products";
+import React, { useEffect, useRef } from "react";
+import useIntersection from "../../hooks/useIntersection";
 
 const ProductListPage = () => {
-  const { data } = useQuery<Products>(QueryKeys.PRODUCTS, () =>
-    graphqlFetcher(GET_PRODUCTS)
-  );
+  const fetchMoreRef = useRef<HTMLDivElement>(null);
+  const intersecting = useIntersection(fetchMoreRef);
+
+  const { data, isSuccess, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useInfiniteQuery<Products>(
+      QueryKeys.PRODUCTS,
+      ({ pageParam = "" }) =>
+        graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
+      {
+        getNextPageParam: (lastPage) => {
+          return lastPage.products[lastPage.products.length - 1]?.id;
+        },
+      }
+    );
+
+  useEffect(() => {
+    if (!intersecting || !isSuccess || !hasNextPage || isFetchingNextPage)
+      return;
+    fetchNextPage();
+  }, [intersecting]);
 
   return (
     <div>
       <h2>상품목록</h2>
-      <ProductList list={data?.products || []} />
+      <ProductList list={data?.pages || []} />
+      <div ref={fetchMoreRef} />
     </div>
   );
 };
--- shopping-mall/server/src/resolvers/product.ts
+++ shopping-mall/server/src/resolvers/product.ts
@@ -1,21 +1,14 @@
 import { Resolver } from "./types";
 
-const mockProducts = Array.from({ length: 20 }).map((_, i) => ({
-  id: i + 1 + "",
-  imageUrl: `http://placeimg.com/640/480/${i + 1}`,
-  price: 50000,
-  title: `임시상품-${i + 1}`,
-  description: `임시상세내용-${i + 1}`,
-  createdAt: new Date(1646745501883 + i * 1000 * 60 * 60 * 10).toString(),
-}));
-
 const productResolver: Resolver = {
   Query: {
-    products: async (parent, args, context, info) => {
-      return mockProducts;
+    products: async (parent, { cursor = "" }, { db }) => {
+      const fromIndex =
+        db.products.findIndex((product) => product.id === cursor) + 1;
+      return db.products.slice(fromIndex, fromIndex + 15) || [];
     },
-    product: async (parent, { id }) => {
-      const found = mockProducts.find((item) => item.id === id);
+    product: async (parent, { id }, { db }) => {
+      const found = db.products.find((item) => item.id === id);
       if (found) return found;
       return null;
     },
