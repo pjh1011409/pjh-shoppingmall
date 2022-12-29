4a7c84c9b34fec2317dbbf0780cb18e67d26ffd5
ParkJeongHo <pjh31208@naver.com>
2022-12-29 17:30:31 +0900

    feat : Add admin page route

--- shopping-mall/client/src/routes.tsx
+++ shopping-mall/client/src/routes.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import GlobalLayout from './pages/_layout'
 
 const DynamicIndex = React.lazy(() => import('./pages/index'));
+const DynamicAdminIndex = React.lazy(() => import('./pages/admin/index'));
 const DynamicCartIndex = React.lazy(() => import('./pages/cart/index'));
 const DynamicPaymentIndex = React.lazy(() => import('./pages/payment/index'));
 const DynamicProductsIndex = React.lazy(() => import('./pages/products/index'));
@@ -15,6 +16,7 @@ export const routes = [
     element: <GlobalLayout />,
     children: [
       { path: '/', element: <DynamicIndex />, index: true},
+      { path: '/admin', element: <DynamicAdminIndex />, index: true},
       { path: '/cart', element: <DynamicCartIndex />, index: true},
       { path: '/payment', element: <DynamicPaymentIndex />, index: true},
       { path: '/products', element: <DynamicProductsIndex />, index: true},
@@ -25,6 +27,7 @@ export const routes = [
 
 export const pages = [
   { route: '/' },
+  { route: '/admin' },
   { route: '/cart' },
   { route: '/payment' },
   { route: '/products' },
