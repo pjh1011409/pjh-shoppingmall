5442b96b6d286479a0bffd3c8ba52068d2785ed0
ParkJeongHo <pjh31208@naver.com>
2022-12-30 14:19:16 +0900

    remove : Delete unnecessary files

--- server/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMeta {
-  env: {
-    VITE_BASE_URL?: string | undefined;
-    VITE_API_KEY?: string | undefined;
-    VITE_SERVER_URL?: string | undefined;
-    VITE_CLIENT_URL?: string | undefined;
-  };
-}
