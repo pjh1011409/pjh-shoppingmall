c51d6594959fa840fb7157ce117eea830a7c7eff
ParkJeongHo <pjh31208@naver.com>
2022-12-31 22:02:19 +0900

    feat : Set dark mode globally

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,19 +3,29 @@ import { routes } from "./routes";
 import { getClient } from "./queryClient";
 import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import NavBar from "./components/common/navBar";
-import React from "react";
+import NavBar from "./components/navigation/navBar";
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
 const App = () => {
   const elem = useRoutes(routes);
   const queryClient = getClient();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <NavBar />
-      {elem}
+      <Background>
+        <NavBar />
+        <div style={{ marginTop: "100px" }} />
+        {elem}
+      </Background>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
 
 export default App;
+
+const Background = styled.div`
+  background-color: var(--background-color);
+  color: var(--primary-color);
+`;
