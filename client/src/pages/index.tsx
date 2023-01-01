0619c1e75c4e2cf2d47283b73837269e2103c26a
ParkJeongHo <pjh31208@naver.com>
2023-01-01 22:20:51 +0900

    feat : Create Main home UI

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,28 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-const MainPage = () => <div>메인페이지!!!!</div>;
+const MainPage = () => (
+  <div>
+    <Row className="homeMain">
+      <Col sm className="mainLayout">
+        <div className="mainHeader">
+          <div className="header1">Freshly pressed,</div>
+          <div className="header1">straight to</div>
+          <div className="header1">your door</div>
+          <p className="header2">
+            At pure Flave, we're dedicated to goodness in every sip. Delicous,
+            fresh jucie, just as nature intended
+          </p>
+          <button className="gotoShopBtn">
+            <div>Shop</div>
+          </button>
+        </div>
+      </Col>
+      <Col sm className="mainLayout">
+        <div className="mainImage">image</div>
+      </Col>
+    </Row>
+  </div>
+);
 export default MainPage;
