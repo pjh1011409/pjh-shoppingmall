2178cd4214a1f80ad504970035021aa76624c99a
ParkJeongHo <pjh31208@naver.com>
2023-01-04 17:00:05 +0900

    design: Design bottomBar UI

--- client/src/components/navigation/bottomBar.tsx
+++ client/src/components/navigation/bottomBar.tsx
@@ -1,30 +1,75 @@
-import Box from "@mui/material/Box";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import EmailIcon from "@mui/icons-material/Email";
-import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import React from "react";
+import { Button, Icon } from "semantic-ui-react";
 
 const BottomBar = () => {
+  const handleCopyClipBoard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+
+      alert("메일을 복사하였습니다.");
+    } catch (error) {
+      alert("복사 실패!");
+    }
+  };
+
   return (
     <div className="bottomBar">
       <div className="boxLayout">
-        <BottomNavigation sx={{ width: 500 }} className="bottomBox">
+        <BottomNavigation sx={{ width: 500, height: 70 }} className="bottomBox">
           <BottomNavigationAction
             label="Mail"
             value="mail"
-            icon={<EmailIcon fontSize="large" />}
+            icon={
+              <Button
+                onClick={() => handleCopyClipBoard("pjh31208@naver.com")}
+                animated="vertical"
+                style={{ backgroundColor: "white", width: "150px" }}
+              >
+                <Button.Content
+                  hidden
+                  style={{ fontSize: "15px", padding: "0px 5px" }}
+                >
+                  pjh31208@naver.com
+                </Button.Content>
+                <Button.Content visible>
+                  <Icon name="mail" style={{ fontSize: "35px" }} />
+                </Button.Content>
+              </Button>
+            }
           />
+
           <BottomNavigationAction
             label="Github"
             value="github"
-            icon={<GitHubIcon fontSize="large" />}
+            href="https://github.com/pjh1011409"
+            style={{ height: "80px" }}
+            icon={
+              <Button animated="vertical" style={{ backgroundColor: "white" }}>
+                <Button.Content hidden style={{ fontSize: "20px" }}>
+                  Github
+                </Button.Content>
+                <Button.Content visible>
+                  <Icon name="github" style={{ fontSize: "35px" }} />
+                </Button.Content>
+              </Button>
+            }
           />
           <BottomNavigationAction
             label="Profile"
             value="profile"
-            icon={<AccountBoxIcon fontSize="large" />}
+            href="https://www.notion.so/Hi-I-m-Park-Jeong-Ho-c95dc3cffa3343758aa4f4b115b99eeb"
+            icon={
+              <Button animated="vertical" style={{ backgroundColor: "white" }}>
+                <Button.Content hidden style={{ fontSize: "20px" }}>
+                  Notion
+                </Button.Content>
+                <Button.Content visible>
+                  <Icon name="user" style={{ fontSize: "35px" }} />
+                </Button.Content>
+              </Button>
+            }
           />
         </BottomNavigation>
       </div>
